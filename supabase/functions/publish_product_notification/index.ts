fa6fa5d7b6f2b97cece6677745d27614ed327224
santiortizm <santyortiz0125@gmail.com>
2025-02-17 17:43:37 -0500

    Edge function push notification, configured

--- supabase/functions/publish_product_notification/index.ts
+++ supabase/functions/publish_product_notification/index.ts
@@ -1,34 +1,37 @@
-import { createClient } from 'npm:@supabase/supabase-js@2'
-import { JWT } from 'npm:google-auth-library@9'
+import {createClient} from "npm:@supabase/supabase-js@2";
+import {JWT} from "npm:google-auth-library@9";
 import serviceAccount from '../service-account.json' with { type: 'json' }
 
-interface Notification {
-  id: string
-  user_id: string
-  body: string
+interface Product {
+  idProducto: string
+  nombreProducto: string
+  cantidad: number
+  descripcion: string
+  maduracion: string
+  fertilizantes: string
+  fechaCosecha: string
+  fechaCaducidad: string
+  precio: number
+  idImagen: string
+  idPropietario: string
 }
 
-interface WebhookPayload {
+interface WebhookPlayload{
   type: 'INSERT'
   table: string
-  record: Notification
+  record: Order
   schema: 'public'
+  old_record: null | Order
 }
 
 const supabase = createClient(
   Deno.env.get('SUPABASE_URL')!,
-  Deno.env.get('SUPABASE_KEY')!
+  Deno.env.get('SUPABASE_ANON_KEY')!
 )
 
-
 Deno.serve(async (req) => {
-  const payload: WebhookPayload = await req.json()
-
-  const { data } = await supabase
-    .from('profiles')
-    .select('fcm_token')
-    .eq('id', payload.record.user_id)
-    .single()
+  const payload: WebhookPlayload = await req.json()
+  const {data} = await supabase.from('usuarios').select('fcm_token').eq('idUsuario',payload.record.idPropietario).single()
 
   const fcmToken = data!.fcm_token as string
 
@@ -37,6 +40,7 @@ Deno.serve(async (req) => {
     privateKey: serviceAccount.private_key,
   })
 
+  
   const res = await fetch(
     `https://fcm.googleapis.com/v1/projects/${serviceAccount.project_id}/messages:send`,
     {
@@ -49,22 +53,23 @@ Deno.serve(async (req) => {
         message: {
           token: fcmToken,
           notification: {
-            title: `Notification from Supabase`,
-            body: payload.record.body,
-          },
-        },
-      }),
+            title: `Public Product Confirmation`,
+            body: `${payload.record.nombreProducto} purchased for \$${payload.record.precio}.`
+          }
+        }
+      }) 
     }
   )
 
   const resData = await res.json()
-  if (res.status < 200 || 299 < res.status) {
+  if(res.status < 200 || 299 < res.status) {
     throw resData
+    
   }
-
-  return new Response(JSON.stringify(resData), {
-    headers: { 'Content-Type': 'application/json' },
-  })
+  return new Response(
+    JSON.stringify(resData),
+    { headers: { "Content-Type": "application/json" } },
+  )
 })
 
 const getAccessToken = ({
